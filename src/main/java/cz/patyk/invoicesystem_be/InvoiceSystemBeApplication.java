bf0df227f61bca3788ba4ef63880dbfc7cc3bd69
Martin Patyk <martin.patyk@telia.no>
2021-09-15 21:36:15 +0200

    Init project

---
+++ src/main/java/cz/patyk/invoicesystem_be/InvoiceSystemBeApplication.java
@@ -0,0 +1,13 @@
+package cz.patyk.invoicesystem_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceSystemBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceSystemBeApplication.class, args);
+    }
+
+}
