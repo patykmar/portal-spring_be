e9e0e97d82aa461832426cc257dc43964285e7da
Martin Patyk <martin.patyk@telia.no>
2021-09-18 10:41:21 +0200

    added field iso3166alpha3, marked not null for name. Create relation with Company

--- src/main/java/cz/patyk/invoicesystem_be/entities/Country.java
+++ src/main/java/cz/patyk/invoicesystem_be/entities/Country.java
@@ -3,11 +3,10 @@ package cz.patyk.invoicesystem_be.entities;
 import lombok.*;
 import org.hibernate.Hibernate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -20,7 +19,16 @@ public class Country implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(name = "iso3166alpha3", nullable = false)
+    private String iso3166alpha3;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "country")
+    @ToString.Exclude
+    private List<Company> companyList = new ArrayList<>();
 
     @Override
     public boolean equals(Object o) {
