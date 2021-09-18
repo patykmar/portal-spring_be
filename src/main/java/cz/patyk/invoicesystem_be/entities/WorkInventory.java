daf4fe93e725d745055c35dc577858fe50ae9056
Martin Patyk <martin.patyk@telia.no>
2021-09-18 10:50:59 +0200

    specify ignoring of empty value on invoice_id, Rewrote field describe to description because describe is key work of MySQL

--- src/main/java/cz/patyk/invoicesystem_be/entities/WorkInventory.java
+++ src/main/java/cz/patyk/invoicesystem_be/entities/WorkInventory.java
@@ -1,6 +1,8 @@
 package cz.patyk.invoicesystem_be.entities;
 
 import lombok.*;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -31,12 +33,13 @@ public class WorkInventory implements Serializable {
     @ToString.Exclude
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @NotFound(action = NotFoundAction.IGNORE)
     @JoinColumn(name = "invoice_id")
     @ToString.Exclude
     private Invoice invoice;
 
-    private String describe;
+    private String description;
     private Date workStart;
     private Date workEnd;
     private float workDuration;
