7f8706fb7b11207ab2b34a53f5b11a32614b11c1
Martin Patyk <martin.patyk@telia.no>
2021-10-04 11:57:20 +0200

    created InvoiceDto

---
+++ src/main/java/cz/patyk/invoicesystem_be/dto/InvoiceDto.java
@@ -0,0 +1,31 @@
+package cz.patyk.invoicesystem_be.dto;
+
+import cz.patyk.invoicesystem_be.entities.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class InvoiceDto {
+    private int id;
+    private Company supplier;
+    private Company subscriber;
+    private PaymentType paymentType;
+    private User userCreated;
+    private List<WorkInventory> workInventoryList = new ArrayList<>();
+    private List<InvoiceItem> invoiceItemList = new ArrayList<>();
+    private String name;
+    private int due;
+    private Date invoiceCreated;
+    private Date dueDate;
+    private Date paymentDate;
+    private String vs;
+    private String ks;
+
+}
