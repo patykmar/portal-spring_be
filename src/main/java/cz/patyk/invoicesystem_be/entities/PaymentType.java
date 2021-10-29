3a7ad24ca69296f4fca63913ef6a3ba9a4510861
Martin Patyk <martin.patyk@telia.no>
2021-10-29 21:20:26 +0200

    implements Serializable

--- src/main/java/cz/patyk/invoicesystem_be/entities/PaymentType.java
+++ src/main/java/cz/patyk/invoicesystem_be/entities/PaymentType.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
@@ -14,7 +15,7 @@ import java.util.Objects;
 @Setter
 @ToString
 @RequiredArgsConstructor
-public class PaymentType {
+public class PaymentType implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
