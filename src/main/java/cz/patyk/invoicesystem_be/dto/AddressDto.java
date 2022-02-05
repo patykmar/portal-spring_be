fbbdef0481d1ed53b68058aa00329a6d6cdd3aa3
Martin Patyk <martin.patyk@telia.no>
2022-02-05 16:17:00 +0100

    created AddressDto

---
+++ src/main/java/cz/patyk/invoicesystem_be/dto/AddressDto.java
@@ -0,0 +1,16 @@
+package cz.patyk.invoicesystem_be.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class AddressDto {
+    private Long id;
+    private CountryDto countryDto;
+    private String street;
+    private String city;
+    private String zipCode;
+}
