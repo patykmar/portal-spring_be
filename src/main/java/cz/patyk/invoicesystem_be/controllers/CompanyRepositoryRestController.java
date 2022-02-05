53541c34048dd6226cbf6d06b97ab3dffb8e4519
Martin Patyk <martin.patyk@telia.no>
2022-02-05 16:11:17 +0100

    forward repository to service layer

--- src/main/java/cz/patyk/invoicesystem_be/controllers/CompanyRepositoryRestController.java
+++ src/main/java/cz/patyk/invoicesystem_be/controllers/CompanyRepositoryRestController.java
@@ -1,29 +1,27 @@
 package cz.patyk.invoicesystem_be.controllers;
 
-import cz.patyk.invoicesystem_be.entities.Company;
-import cz.patyk.invoicesystem_be.repositories.CompanyRepository;
-import lombok.AllArgsConstructor;
+import cz.patyk.invoicesystem_be.dto.out.CompanyDtoOut;
+import cz.patyk.invoicesystem_be.service.CompanyService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RepositoryRestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CompanyRepositoryRestController {
-    private final CompanyRepository companyRepository;
+    @NonNull
+    private final CompanyService companyService;
 
     @GetMapping(value = "/companies")
-    public @ResponseBody ResponseEntity<CollectionModel<Company>> getCompanies() {
-        List<Company> companies = companyRepository.findAll();
-
-        CollectionModel<Company> companiesCollections = CollectionModel.of(companies);
+    public @ResponseBody ResponseEntity<CollectionModel<CompanyDtoOut>> getCompanies() {
+        CollectionModel<CompanyDtoOut> companiesCollections = CollectionModel.of(companyService.getAllCompanies());
 
         companiesCollections.add(linkTo(methodOn(CompanyRepositoryRestController.class).getCompanies()).withSelfRel());
 
