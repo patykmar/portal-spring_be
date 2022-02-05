63aec021c3f64398a6b395b050a913fc5e5e5710
Martin Patyk <martin.patyk@telia.no>
2022-02-05 16:22:00 +0100

    change date type from int to Long of ID field

--- src/main/java/cz/patyk/invoicesystem_be/repositories/CountryRepository.java
+++ src/main/java/cz/patyk/invoicesystem_be/repositories/CountryRepository.java
@@ -3,5 +3,5 @@ package cz.patyk.invoicesystem_be.repositories;
 import cz.patyk.invoicesystem_be.entities.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CountryRepository extends JpaRepository<Country, Integer> {
+public interface CountryRepository extends JpaRepository<Country, Long> {
 }
