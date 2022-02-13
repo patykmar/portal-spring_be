6d8d3247b97768c933b36f7a2ed9ef261d99cdd8
Martin Patyk <martin.patyk@telia.no>
2022-02-13 19:57:50 +0100

    created VatController

---
+++ src/main/java/cz/patyk/invoicesystem_be/controllers/VatController.java
@@ -0,0 +1,24 @@
+package cz.patyk.invoicesystem_be.controllers;
+
+import cz.patyk.invoicesystem_be.dto.VatDto;
+import cz.patyk.invoicesystem_be.service.VatService;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest-manual")
+@AllArgsConstructor
+public class VatController {
+    @NonNull
+    private final VatService vatService;
+
+    @GetMapping("/simple-vats")
+    public List<VatDto> findAll(){
+        return vatService.getAllVats();
+    }
+}
