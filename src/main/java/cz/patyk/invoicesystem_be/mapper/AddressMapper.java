dfee495a371aaaa3354e2db05cfe9fefe1db2dd1
Ing. Martin Patyk <martin@patyk.cz>
2022-02-21 23:03:48 +0100

    added method for get country from database

--- src/main/java/cz/patyk/invoicesystem_be/mapper/AddressMapper.java
+++ src/main/java/cz/patyk/invoicesystem_be/mapper/AddressMapper.java
@@ -1,16 +1,28 @@
 package cz.patyk.invoicesystem_be.mapper;
 
+import cz.patyk.invoicesystem_be.dto.in.AddressDtoIn;
 import cz.patyk.invoicesystem_be.entities.Address;
 import cz.patyk.invoicesystem_be.dto.AddressDto;
+import cz.patyk.invoicesystem_be.entities.Country;
+import cz.patyk.invoicesystem_be.repositories.CountryRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring", uses = {CountryMapper.class})
-public interface AddressMapper {
+public abstract class AddressMapper {
+
+    @Autowired
+    protected CountryRepository countryRepository;
 
     @Mapping(target = "countryId", source = "country.id")
     @Mapping(target = "countryDto", source = "address.country")
-    AddressDto toDto(Address address);
+    public abstract AddressDto toDto(Address address);
 
-    Address toEntity(AddressDto addressDto);
+    @Mapping(target = "country", expression = "java(getCountry(addressDto.getCountry()))")
+    public abstract Address toEntity(AddressDtoIn addressDto);
+
+    public Country getCountry(Long country) {
+        return countryRepository.getById(country);
+    }
 }
