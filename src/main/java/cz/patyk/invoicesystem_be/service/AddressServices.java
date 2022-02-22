21707f21392047e82bf0ebebd5c54a320489f824
Martin Patyk <79904068+patykmar@users.noreply.github.com>
2022-02-22 22:12:44 +0100

    Created Address controller and service (#9)

--- src/main/java/cz/patyk/invoicesystem_be/service/AddressServices.java
+++ src/main/java/cz/patyk/invoicesystem_be/service/AddressServices.java
@@ -1,6 +1,8 @@
 package cz.patyk.invoicesystem_be.service;
 
 import cz.patyk.invoicesystem_be.dto.AddressDto;
+import cz.patyk.invoicesystem_be.dto.in.AddressDtoIn;
+import cz.patyk.invoicesystem_be.entities.Address;
 import cz.patyk.invoicesystem_be.mapper.AddressMapper;
 import cz.patyk.invoicesystem_be.repositories.AddressRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,15 +16,30 @@ public class AddressServices {
     private final AddressRepository addressRepository;
     private final AddressMapper addressMapper;
 
-    public List<AddressDto> getAllAddresses(){
+    public List<AddressDto> getAllAddresses() {
         return addressRepository.findAll()
                 .stream()
                 .map(addressMapper::toDto)
                 .toList();
     }
 
-    public AddressDto getAddress(Long id){
+    public AddressDto getAddress(Long id) {
         return addressMapper
                 .toDto(addressRepository.getById(id));
+    }
+
+    public AddressDto newAddress(AddressDtoIn addressDtoIn) {
+        Address address = addressMapper.toEntity(addressDtoIn);
+        return addressMapper.toDto(addressRepository.save(address));
+    }
+
+    public AddressDto edit(AddressDtoIn addressDtoIn, Long id) {
+        Address address = addressMapper.toEntity(addressDtoIn);
+        address.setId(id);
+        return addressMapper.toDto(addressRepository.save(address));
+    }
+
+    public void delete(Long id) {
+        addressRepository.deleteById(id);
     }
 }
