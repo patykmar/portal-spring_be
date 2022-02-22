13787165b142d68594a2697455dddb93b2318431
Ing. Martin Patyk <patyk.m@gmail.com>
2022-02-22 22:08:28 +0100

    created method getAddress,newAddress,edit and delete

--- src/main/java/cz/patyk/invoicesystem_be/controllers/AddressController.java
+++ src/main/java/cz/patyk/invoicesystem_be/controllers/AddressController.java
@@ -1,10 +1,16 @@
 package cz.patyk.invoicesystem_be.controllers;
 
 import cz.patyk.invoicesystem_be.dto.AddressDto;
+import cz.patyk.invoicesystem_be.dto.in.AddressDtoIn;
 import cz.patyk.invoicesystem_be.service.AddressServices;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +24,28 @@ public class AddressController {
     @GetMapping("")
     public List<AddressDto> getAllAddresses() {
         return addressServices.getAllAddresses();
+    }
+
+    @GetMapping("/{id}")
+    public AddressDto getAddress(@PathVariable Long id) {
+        return addressServices.getAddress(id);
+    }
+
+    @PostMapping("")
+    public AddressDto newAddress(@RequestBody AddressDtoIn addressDtoIn) {
+        return addressServices.newAddress(addressDtoIn);
+    }
+
+    @PutMapping("/{id}")
+    public AddressDto edit(
+            @RequestBody AddressDtoIn addressDtoIn,
+            @PathVariable Long id
+    ) {
+        return addressServices.edit(addressDtoIn, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        addressServices.delete(id);
     }
 }
