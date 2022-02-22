c8dd6558f8f0d8942d42c3801424b6c04efb0473
Martin Patyk <79904068+patykmar@users.noreply.github.com>
2022-02-23 00:21:00 +0100

    create simple exception handling (#11)

---
+++ src/main/java/cz/patyk/invoicesystem_be/dto/ErrorDto.java
@@ -0,0 +1,14 @@
+package cz.patyk.invoicesystem_be.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDto {
+    private String message;
+}
