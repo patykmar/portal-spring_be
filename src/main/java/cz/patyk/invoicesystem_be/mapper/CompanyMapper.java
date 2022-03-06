f9b853b7a3d6193365774d32ed039c9345bbb87d
Ing. Martin Patyk <patyk.m@gmail.com>
2022-03-06 23:43:19 +0100

    rewrite mapper from interface to abstract class

--- src/main/java/cz/patyk/invoicesystem_be/mapper/CompanyMapper.java
+++ src/main/java/cz/patyk/invoicesystem_be/mapper/CompanyMapper.java
@@ -1,14 +1,29 @@
 package cz.patyk.invoicesystem_be.mapper;
 
+import cz.patyk.invoicesystem_be.entities.Address;
+import cz.patyk.invoicesystem_be.repositories.AddressRepository;
 import org.mapstruct.Mapper;
 
 import cz.patyk.invoicesystem_be.dto.in.CompanyDtoIn;
 import cz.patyk.invoicesystem_be.dto.out.CompanyDtoOut;
 import cz.patyk.invoicesystem_be.entities.Company;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring")
-public interface CompanyMapper {
-	Company toEntity(CompanyDtoIn companyDtoIn);
-	
-	CompanyDtoOut toDto(Company company);
+@Mapper(componentModel = "spring", uses = {AddressMapper.class, CountryMapper.class})
+public abstract class CompanyMapper {
+
+	@Autowired
+	protected AddressRepository addressRepository;
+
+	@Mapping(target = "address", source = "address.id")
+	@Mapping(target = "addressDtoOut", source = "company.address")
+	public abstract CompanyDtoOut toDto(Company company);
+
+	@Mapping(target = "address", expression = "java(getAddress(companyDtoIn.getAddress()))")
+	public abstract Company toEntity(CompanyDtoIn companyDtoIn);
+
+	public Address getAddress(Long address){
+		return addressRepository.getById(address);
+	}
 }
