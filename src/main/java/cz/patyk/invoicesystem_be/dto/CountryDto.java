ebe6ed049f5cf6a153b282ce1208e0b5cbe0544d
Martin Patyk <martin.patyk@telia.no>
2022-03-06 15:26:09 +0100

    added validation constraints

--- src/main/java/cz/patyk/invoicesystem_be/dto/CountryDto.java
+++ src/main/java/cz/patyk/invoicesystem_be/dto/CountryDto.java
@@ -3,12 +3,20 @@ package cz.patyk.invoicesystem_be.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Data
-@AllArgsConstructor
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CountryDto {
+    @Positive
     private Long id;
+    @NotNull
     private String name;
+    @NotNull
     private String iso3166alpha3;
 }
