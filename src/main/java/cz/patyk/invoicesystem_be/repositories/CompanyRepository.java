cf1b060c93fca8bd19b8eeb02a55449b6932ffff
Martin Patyk <martin.patyk@telia.no>
2022-03-16 21:24:13 +0100

    fix type of ID

--- src/main/java/cz/patyk/invoicesystem_be/repositories/CompanyRepository.java
+++ src/main/java/cz/patyk/invoicesystem_be/repositories/CompanyRepository.java
@@ -3,5 +3,5 @@ package cz.patyk.invoicesystem_be.repositories;
 import cz.patyk.invoicesystem_be.entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CompanyRepository extends JpaRepository<Company, Integer> {
+public interface CompanyRepository extends JpaRepository<Company, Long> {
 }
