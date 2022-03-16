707da1bd3840d954bc640de7095b5ddacd79fdf1
Martin Patyk <martin.patyk@telia.no>
2022-03-16 21:28:10 +0100

    added methods for manipulation with company data

--- src/main/java/cz/patyk/invoicesystem_be/service/CompanyService.java
+++ src/main/java/cz/patyk/invoicesystem_be/service/CompanyService.java
@@ -1,9 +1,10 @@
 package cz.patyk.invoicesystem_be.service;
 
+import cz.patyk.invoicesystem_be.dto.in.CompanyDtoIn;
 import cz.patyk.invoicesystem_be.dto.out.CompanyDtoOut;
+import cz.patyk.invoicesystem_be.entities.Company;
 import cz.patyk.invoicesystem_be.mapper.CompanyMapper;
 import cz.patyk.invoicesystem_be.repositories.CompanyRepository;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,15 +14,44 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class CompanyService {
-    @NonNull
-    private final CompanyRepository companyRepository;
-    @NonNull
-    private final CompanyMapper companyMapper;
+    private static final String NOT_FOUND_MESSAGE = "Company not found";
 
-    public List<CompanyDtoOut> getAllCompanies(){
+    private final CompanyRepository companyRepository;
+    private final CompanyMapper companyMapper;
+    private final ErrorHandleService errorHandleService;
+
+    public List<CompanyDtoOut> getAllCompanies() {
         return companyRepository.findAll()
                 .stream()
                 .map(companyMapper::toDto)
                 .collect(Collectors.toList());
+    }
+
+    public CompanyDtoOut getCompany(Long id) {
+        return companyMapper.toDto(
+                companyRepository.findById(id)
+                        .orElseThrow(() -> errorHandleService.handleNotFoundError(id, NOT_FOUND_MESSAGE))
+        );
+    }
+
+    public CompanyDtoOut newCompany(CompanyDtoIn companyDtoIn) {
+        Company company = companyMapper.toEntity(companyDtoIn);
+        return companyMapper.toDto(companyRepository.save(company));
+    }
+
+    public CompanyDtoOut edit(CompanyDtoIn companyDtoIn, Long id) {
+        if (!companyRepository.existsById(id)) {
+            throw errorHandleService.handleNotFoundError(id, NOT_FOUND_MESSAGE);
+        }
+        Company company = companyMapper.toEntity(companyDtoIn);
+        company.setId(id);
+        return companyMapper.toDto(companyRepository.save(company));
+    }
+
+    public void delete(Long id) {
+        if (!companyRepository.existsById(id)) {
+            throw errorHandleService.handleNotFoundError(id, NOT_FOUND_MESSAGE);
+        }
+        companyRepository.deleteById(id);
     }
 }
