f878a841c5036b92385454e3ff3ec7105164dc0e
Martin Patyk <martin.patyk@telia.no>
2022-03-21 22:47:01 +0100

    created CompanyController

---
+++ src/main/java/cz/patyk/invoicesystem_be/controllers/CompanyController.java
@@ -0,0 +1,52 @@
+package cz.patyk.invoicesystem_be.controllers;
+
+import cz.patyk.invoicesystem_be.dto.in.CompanyDtoIn;
+import cz.patyk.invoicesystem_be.dto.out.CompanyDtoOut;
+import cz.patyk.invoicesystem_be.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
+@RequiredArgsConstructor
+public class CompanyController {
+    private final CompanyService companyService;
+
+    @GetMapping("")
+    public ResponseEntity<List<CompanyDtoOut>> getAll() {
+        return ResponseEntity.ok(companyService.getAllCompanies());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CompanyDtoOut> get(@PathVariable Long id) {
+        return ResponseEntity.ok(companyService.getCompany(id));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<CompanyDtoOut> newCompany(
+            @RequestBody CompanyDtoIn companyDtoIn
+    ) {
+        return ResponseEntity.ok(
+                companyService.newCompany(companyDtoIn)
+        );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CompanyDtoOut> edit(
+            @RequestBody CompanyDtoIn companyDtoIn,
+            @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(
+                companyService.edit(companyDtoIn, id)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        companyService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
