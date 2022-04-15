a163e4e4c5ad9a775f82490682a1a4cc35f278e7
Martin Patyk <79904068+patykmar@users.noreply.github.com>
2022-04-15 14:51:08 +0200

    AddressController Enable pageable and Hateoas links (#16)

--- src/main/java/cz/patyk/invoicesystem_be/dto/out/AddressDtoOut.java
+++ src/main/java/cz/patyk/invoicesystem_be/dto/out/AddressDtoOut.java
@@ -1,18 +1,35 @@
 package cz.patyk.invoicesystem_be.dto.out;
 
 import cz.patyk.invoicesystem_be.dto.CountryDto;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
 
-@Data
-@AllArgsConstructor
+import java.util.Objects;
+
+@Getter
+@Setter
 @Builder
-public class AddressDtoOut {
+@AllArgsConstructor
+public class AddressDtoOut extends RepresentationModel<AddressDtoOut> {
     private Long id;
     private CountryDto countryDto;
     private Long country;
     private String street;
     private String city;
     private String zipCode;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        AddressDtoOut that = (AddressDtoOut) o;
+        return Objects.equals(id, that.id) && Objects.equals(countryDto, that.countryDto) && Objects.equals(country, that.country) && Objects.equals(street, that.street) && Objects.equals(city, that.city) && Objects.equals(zipCode, that.zipCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, countryDto, country, street, city, zipCode);
+    }
 }
+
