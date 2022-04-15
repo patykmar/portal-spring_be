4c5b0c07f5bbec39dec7f686e28a9e906a5af462
Martin Patyk <martin.patyk@telia.no>
2022-04-16 00:40:06 +0200

    update test, expecting calculate multiplier

--- src/test/java/cz/patyk/invoicesystem_be/mapper/VatMapperTest.java
+++ src/test/java/cz/patyk/invoicesystem_be/mapper/VatMapperTest.java
@@ -1,6 +1,7 @@
 package cz.patyk.invoicesystem_be.mapper;
 
-import cz.patyk.invoicesystem_be.dto.VatDto;
+import cz.patyk.invoicesystem_be.dto.in.VatDtoIn;
+import cz.patyk.invoicesystem_be.dto.out.VatDtoOut;
 import cz.patyk.invoicesystem_be.entities.Vat;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -20,11 +21,11 @@ class VatMapperTest {
 
     @ParameterizedTest
     @MethodSource("providerDtos")
-    void toEntity(VatDto vatDto, Long idExpected, int intValuesExpected, boolean booleanExpected) {
-        assertThat(VAT_MAPPER.toEntity(vatDto))
+    void toEntity(VatDtoIn vatDtoIn, Long idExpected, int intValuesExpected, boolean booleanExpected) {
+        assertThat(VAT_MAPPER.toEntity(vatDtoIn))
                 .returns(idExpected, Vat::getId)
                 .returns(intValuesExpected, Vat::getPercent)
-                .returns(intValuesExpected, Vat::getMultiplier)
+                .returns(intValuesExpected + Vat.MULTIPLIER_CONST, Vat::getMultiplier)
                 .returns(booleanExpected, Vat::isDefault);
     }
 
@@ -32,10 +33,10 @@ class VatMapperTest {
     @MethodSource("providerEntities")
     void toDto(Vat vat, Long idExpected, int intValuesExpected, boolean booleanExpected) {
         assertThat(VAT_MAPPER.toDto(vat))
-                .returns(idExpected, VatDto::getId)
-                .returns(intValuesExpected, VatDto::getPercent)
-                .returns(intValuesExpected, VatDto::getMultiplier)
-                .returns(booleanExpected, VatDto::isDefault);
+                .returns(idExpected, VatDtoOut::getId)
+                .returns(intValuesExpected, VatDtoOut::getPercent)
+                .returns(intValuesExpected, VatDtoOut::getMultiplier)
+                .returns(booleanExpected, VatDtoOut::isDefault);
     }
 
     private static Stream<Arguments> providerEntities() {
@@ -50,11 +51,11 @@ class VatMapperTest {
 
     private static Stream<Arguments> providerDtos() {
         return Stream.of(
-                Arguments.of(VatDto.builder().id(LONG_MIN).name(TEST_VAT_NAME).percent(INT_MIN).multiplier(INT_MIN).isDefault(false).build(), LONG_MIN, INT_MIN, false),
-                Arguments.of(VatDto.builder().id(LONG_NEGATIVE).name(TEST_VAT_NAME).percent(INT_NEGATIVE).multiplier(INT_NEGATIVE).isDefault(true).build(), LONG_NEGATIVE, INT_NEGATIVE, true),
-                Arguments.of(VatDto.builder().id(LONG_ZERO).name(TEST_VAT_NAME).percent(INT_ZERO).multiplier(INT_ZERO).isDefault(false).build(), LONG_ZERO, INT_ZERO, false),
-                Arguments.of(VatDto.builder().id(LONG_POSITIVE).name(TEST_VAT_NAME).percent(INT_POSITIVE).multiplier(INT_POSITIVE).isDefault(true).build(), LONG_POSITIVE, INT_POSITIVE, true),
-                Arguments.of(VatDto.builder().id(LONG_MAX).name(TEST_VAT_NAME).percent(INT_MAX).multiplier(INT_MAX).isDefault(false).build(), LONG_MAX, INT_MAX, false)
+                Arguments.of(VatDtoIn.builder().id(LONG_MIN).name(TEST_VAT_NAME).percent(INT_MIN).isDefault(false).build(), LONG_MIN, INT_MIN, false),
+                Arguments.of(VatDtoIn.builder().id(LONG_NEGATIVE).name(TEST_VAT_NAME).percent(INT_NEGATIVE).isDefault(true).build(), LONG_NEGATIVE, INT_NEGATIVE, true),
+                Arguments.of(VatDtoIn.builder().id(LONG_ZERO).name(TEST_VAT_NAME).percent(INT_ZERO).isDefault(false).build(), LONG_ZERO, INT_ZERO, false),
+                Arguments.of(VatDtoIn.builder().id(LONG_POSITIVE).name(TEST_VAT_NAME).percent(INT_POSITIVE).isDefault(true).build(), LONG_POSITIVE, INT_POSITIVE, true),
+                Arguments.of(VatDtoIn.builder().id(LONG_MAX).name(TEST_VAT_NAME).percent(INT_MAX).isDefault(false).build(), LONG_MAX, INT_MAX, false)
         );
     }
 }
