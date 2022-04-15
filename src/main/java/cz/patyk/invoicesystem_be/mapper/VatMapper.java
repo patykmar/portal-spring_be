8f51dfc2d73e4f35722ee4c00c685dc3713625fc
Martin Patyk <martin.patyk@telia.no>
2022-04-15 20:37:42 +0200

    specify boolean property for generate mapping

--- src/main/java/cz/patyk/invoicesystem_be/mapper/VatMapper.java
+++ src/main/java/cz/patyk/invoicesystem_be/mapper/VatMapper.java
@@ -3,10 +3,13 @@ package cz.patyk.invoicesystem_be.mapper;
 import cz.patyk.invoicesystem_be.dto.VatDto;
 import cz.patyk.invoicesystem_be.entities.Vat;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface VatMapper {
+    @Mapping(source = "default", target = "isDefault")
     Vat toEntity(VatDto vatDto);
 
+    @Mapping(source = "default", target = "isDefault")
     VatDto toDto(Vat vat);
 }
