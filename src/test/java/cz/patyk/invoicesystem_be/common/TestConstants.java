45bbc6fa99fee872647080dbfa874762fb344129
Martin Patyk <martin.patyk@telia.no>
2022-04-16 17:03:39 +0200

    update Vat constants

--- src/test/java/cz/patyk/invoicesystem_be/common/TestConstants.java
+++ src/test/java/cz/patyk/invoicesystem_be/common/TestConstants.java
@@ -1,5 +1,7 @@
 package cz.patyk.invoicesystem_be.common;
 
+import cz.patyk.invoicesystem_be.entities.Vat;
+
 public class TestConstants {
     public static final Long LONG_MIN = Long.MIN_VALUE;
     public static final Long LONG_NEGATIVE = -1L;
@@ -14,5 +16,9 @@ public class TestConstants {
     public static final int INT_MAX = Integer.MAX_VALUE;
 
     public static final String TEST_VAT_NAME = "test vat name";
+    public static final String TEST_TARIFF_NAME = "test tariff name";
 
+    public static final int FK_VAT_PERCENT = 20;
+    public static final int FK_VAT_MULTIPLIER = 120;
+    public static final Vat FK_VAT = Vat.builder().id(LONG_POSITIVE).name(TEST_VAT_NAME).isDefault(true).percent(FK_VAT_PERCENT).multiplier(FK_VAT_MULTIPLIER).build();
 }
