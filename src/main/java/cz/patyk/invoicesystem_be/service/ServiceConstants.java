3cfb19c8d292abe7548102c9f9927183ccf5ba53
Martin Patyk <martin.patyk@telia.no>
2022-04-17 12:01:50 +0200

    created ServiceConstants

---
+++ src/main/java/cz/patyk/invoicesystem_be/service/ServiceConstants.java
@@ -0,0 +1,9 @@
+package cz.patyk.invoicesystem_be.service;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ServiceConstants {
+    public static final String TARIFF_NOT_FOUND_MESSAGE = "Tariff not found";
+}
