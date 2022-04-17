5cf61998356f958702299a77f23b5247b82753a9
Martin Patyk <martin.patyk@telia.no>
2022-04-17 18:25:42 +0200

    consolidate constants

--- src/test/java/cz/patyk/invoicesystem_be/common/TestConstants.java
+++ src/test/java/cz/patyk/invoicesystem_be/common/TestConstants.java
@@ -1,24 +1,54 @@
 package cz.patyk.invoicesystem_be.common;
 
+import cz.patyk.invoicesystem_be.dto.CountryDto;
+import cz.patyk.invoicesystem_be.dto.out.AddressDtoOut;
+import cz.patyk.invoicesystem_be.entities.Address;
+import cz.patyk.invoicesystem_be.entities.Country;
 import cz.patyk.invoicesystem_be.entities.Vat;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TestConstants {
-    public static final Long LONG_MIN = Long.MIN_VALUE;
-    public static final Long LONG_NEGATIVE = -1L;
-    public static final Long LONG_ZERO = 0L;
-    public static final Long LONG_POSITIVE = 1L;
-    public static final Long LONG_MAX = Long.MAX_VALUE;
 
-    public static final int INT_MIN = Integer.MIN_VALUE;
     public static final int INT_NEGATIVE = -1;
     public static final int INT_ZERO = 0;
     public static final int INT_POSITIVE = 1;
-    public static final int INT_MAX = Integer.MAX_VALUE;
 
-    public static final String TEST_VAT_NAME = "test vat name";
-    public static final String TEST_TARIFF_NAME = "test tariff name";
+    public static final Long LONG_NEGATIVE = -1L;
+    public static final Long LONG_ZERO = 0L;
+    public static final Long LONG_POSITIVE = 1L;
 
-    public static final int FK_VAT_PERCENT = 20;
-    public static final int FK_VAT_MULTIPLIER = 120;
-    public static final Vat FK_VAT = Vat.builder().id(LONG_POSITIVE).name(TEST_VAT_NAME).isDefault(true).percent(FK_VAT_PERCENT).multiplier(FK_VAT_MULTIPLIER).build();
+    public static final String COMPANY_TEST_NAME = "Testing company name";
+    public static final String COMPANY_TEST_DESCRIPTION = "Testing company description";
+    public static final String COMPANY_TEST_COMPANY_ID = "123456789";
+    public static final String COMPANY_TEST_VAT_NUMBER = "CZ123456789";
+    public static final Long COMPANY_TEST_CREATED = 915148800L;
+    public static final Long COMPANY_TEST_MODIFY = Instant.now().getEpochSecond();
+    public static final String COMPANY_TEST_ACCOUNT_NUMBER = "12345-123456789/0123";
+    public static final String COMPANY_TEST_IBAN = "CZ1234567890123456789012";
+
+    public static final String COUNTRY_TEST_NAME = "Val Verde";
+    public static final String COUNTRY_TEST_ISO_3166_ALPHA_3 = "VV";
+    public static final Country COUNTRY_TEST_ENTITY = new Country(Long.MAX_VALUE, COUNTRY_TEST_NAME, COUNTRY_TEST_ISO_3166_ALPHA_3, List.of());
+    public static final CountryDto COUNTRY_TEST_DTO = CountryDto.builder()
+            .id(Long.MAX_VALUE).name(COUNTRY_TEST_NAME).iso3166alpha3(COUNTRY_TEST_ISO_3166_ALPHA_3).build();
+
+    public static final String ADDRESS_TEST_STREET = "Fake street 123";
+    public static final String ADDRESS_TEST_CITY = "Springfield";
+    public static final String ADDRESS_TEST_ZIP_CODE = "12345";
+    public static final Address ADDRESS_TEST_ENTITY = new Address(Long.MAX_VALUE, ADDRESS_TEST_STREET, ADDRESS_TEST_CITY, ADDRESS_TEST_ZIP_CODE, COUNTRY_TEST_ENTITY);
+    public static final AddressDtoOut ADDRESS_TEST_DTO_OUT = AddressDtoOut.builder()
+            .id(Long.MAX_VALUE).street(ADDRESS_TEST_STREET).city(ADDRESS_TEST_CITY).zipCode(ADDRESS_TEST_ZIP_CODE).countryDto(COUNTRY_TEST_DTO).build();
+
+    public static final String VAT_TEST_NAME = "test vat name";
+
+    public static final int VAT_TEST_PERCENT = 20;
+    public static final int VAT_TEST_MULTIPLIER = 120;
+    public static final Vat VAT_TEST_ENTITY = Vat.builder().id(LONG_POSITIVE).name(VAT_TEST_NAME).isDefault(true).percent(VAT_TEST_PERCENT).multiplier(VAT_TEST_MULTIPLIER).build();
+
+    public static final String TARIFF_TEST_NAME = "test tariff name";
 }
