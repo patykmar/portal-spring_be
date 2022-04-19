c84b3d05f74e23fcf0292a3421c9e05ec6bb193f
Martin Patyk <martin.patyk@telia.no>
2022-04-19 16:04:41 +0200

    updated messages

--- src/main/java/cz/patyk/invoicesystem_be/service/ServiceConstants.java
+++ src/main/java/cz/patyk/invoicesystem_be/service/ServiceConstants.java
@@ -8,6 +8,8 @@ public class ServiceConstants {
     public static final String ADDRESS_NOT_FOUND_MESSAGE = "Address not found";
     public static final String COMPANY_NOT_FOUND_MESSAGE = "Company not found";
     public static final String COUNTRY_NOT_FOUND_MESSAGE = "Country not found";
+    public static final String GENERAL_STATE_NOT_FOUND_MESSAGE = "General state not found";
+    public static final String QUEUE_NOT_FOUND_MESSAGE = "Queue not found";
     public static final String TARIFF_NOT_FOUND_MESSAGE = "Tariff not found";
     public static final String VAT_NOT_FOUND_MESSAGE = "Vat not found";
 }
