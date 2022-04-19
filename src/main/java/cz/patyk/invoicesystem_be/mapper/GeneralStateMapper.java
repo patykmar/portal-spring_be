417a26114394cf694fd8f6bc0bdae15a6126ac1d
Martin Patyk <martin.patyk@telia.no>
2022-04-19 09:23:56 +0200

    specify mapper

--- src/main/java/cz/patyk/invoicesystem_be/mapper/GeneralStateMapper.java
+++ src/main/java/cz/patyk/invoicesystem_be/mapper/GeneralStateMapper.java
@@ -3,10 +3,17 @@ package cz.patyk.invoicesystem_be.mapper;
 import cz.patyk.invoicesystem_be.dto.GeneralStateDto;
 import cz.patyk.invoicesystem_be.entities.GeneralState;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface GeneralStateMapper {
+    @Mapping(source = "forTicket", target = "isForTicket")
+    @Mapping(source = "forCi", target = "isForCi")
+    @Mapping(source = "forCloseState", target = "isForCloseState")
     GeneralState toEntity(GeneralStateDto generalStateDto);
 
+    @Mapping(source = "forTicket", target = "isForTicket")
+    @Mapping(source = "forCi", target = "isForCi")
+    @Mapping(source = "forCloseState", target = "isForCloseState")
     GeneralStateDto toDto(GeneralState generalState);
 }
