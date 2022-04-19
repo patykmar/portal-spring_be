239a7d4a1c3e44cff2bcfdbf03b242d5db692705
Martin Patyk <martin.patyk@telia.no>
2022-04-19 22:36:10 +0200

    updated InfluencingTicketMapper

--- src/main/java/cz/patyk/invoicesystem_be/mapper/InfluencingTicketMapper.java
+++ src/main/java/cz/patyk/invoicesystem_be/mapper/InfluencingTicketMapper.java
@@ -3,10 +3,15 @@ package cz.patyk.invoicesystem_be.mapper;
 import cz.patyk.invoicesystem_be.dto.InfluencingTicketDto;
 import cz.patyk.invoicesystem_be.entities.InfluencingTicket;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface InfluencingTicketMapper {
+    @Mapping(source = "forPriority", target = "isForPriority")
+    @Mapping(source = "forImpact", target = "isForImpact")
     InfluencingTicketDto toDto(InfluencingTicket influencingTicket);
 
+    @Mapping(source = "forPriority", target = "isForPriority")
+    @Mapping(source = "forImpact", target = "isForImpact")
     InfluencingTicket toEntity(InfluencingTicketDto influencingTicketDto);
 }
