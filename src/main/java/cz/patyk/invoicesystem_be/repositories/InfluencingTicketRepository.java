501e616cbc4b1585078f1b01930e98f297b04ce5
Martin Patyk <martin.patyk@telia.no>
2022-04-20 23:33:41 +0200

    created method for get all priority and impacts

--- src/main/java/cz/patyk/invoicesystem_be/repositories/InfluencingTicketRepository.java
+++ src/main/java/cz/patyk/invoicesystem_be/repositories/InfluencingTicketRepository.java
@@ -1,7 +1,13 @@
 package cz.patyk.invoicesystem_be.repositories;
 
 import cz.patyk.invoicesystem_be.entities.InfluencingTicket;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InfluencingTicketRepository extends JpaRepository<InfluencingTicket, Long> {
+    List<InfluencingTicket> findAllByIsForPriorityIsTrue(Pageable pageable);
+
+    List<InfluencingTicket> findAllByIsForImpactIsTrue(Pageable pageable);
 }
