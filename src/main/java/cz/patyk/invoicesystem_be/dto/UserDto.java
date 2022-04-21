d1b92da9e23fa27a13f16b47a4b7ac561c04764d
Martin Patyk <martin.patyk@telia.no>
2022-04-21 21:38:44 +0200

    updated UserDto

--- src/main/java/cz/patyk/invoicesystem_be/dto/UserDto.java
+++ src/main/java/cz/patyk/invoicesystem_be/dto/UserDto.java
@@ -2,21 +2,38 @@ package cz.patyk.invoicesystem_be.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
-import java.util.Date;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
-public class UserDto {
+public class UserDto extends RepresentationModel<UserDto> {
     private Long id;
     private Long employeeOfCompanyId;
     private String email;
     private String firstName;
     private String lastName;
-    private Date lastLogin;
-    private Date created;
-    private Date passwordChanged;
+    private Long lastLogin;
+    private Long created;
+    private Long passwordChanged;
     private String roles;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        UserDto userDto = (UserDto) o;
+        return Objects.equals(id, userDto.id) && Objects.equals(employeeOfCompanyId, userDto.employeeOfCompanyId) && Objects.equals(email, userDto.email) && Objects.equals(firstName, userDto.firstName) && Objects.equals(lastName, userDto.lastName) && Objects.equals(lastLogin, userDto.lastLogin) && Objects.equals(created, userDto.created) && Objects.equals(passwordChanged, userDto.passwordChanged) && Objects.equals(roles, userDto.roles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, employeeOfCompanyId, email, firstName, lastName, lastLogin, created, passwordChanged, roles);
+    }
 }
