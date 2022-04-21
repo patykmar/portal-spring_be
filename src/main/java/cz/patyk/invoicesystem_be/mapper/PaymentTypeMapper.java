ba8f98cc61aced29da5433a511c55a86b165ab16
Martin Patyk <martin.patyk@telia.no>
2022-04-21 21:40:07 +0200

    updated PaymentTypeMapper

--- src/main/java/cz/patyk/invoicesystem_be/mapper/PaymentTypeMapper.java
+++ src/main/java/cz/patyk/invoicesystem_be/mapper/PaymentTypeMapper.java
@@ -3,10 +3,13 @@ package cz.patyk.invoicesystem_be.mapper;
 import cz.patyk.invoicesystem_be.dto.PaymentTypeDto;
 import cz.patyk.invoicesystem_be.entities.PaymentType;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PaymentTypeMapper {
+    @Mapping(source = "default", target = "isDefault")
     PaymentTypeDto toDto(PaymentType paymentType);
 
+    @Mapping(source = "default", target = "isDefault")
     PaymentType toEntity(PaymentTypeDto paymentTypeDto);
 }
