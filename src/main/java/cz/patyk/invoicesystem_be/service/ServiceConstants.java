b0b713a2ea9bdbe766b439f113b493893ae21787
Martin Patyk <martin.patyk@telia.no>
2022-04-21 22:23:03 +0200

    updated ServiceConstants

--- src/main/java/cz/patyk/invoicesystem_be/service/ServiceConstants.java
+++ src/main/java/cz/patyk/invoicesystem_be/service/ServiceConstants.java
@@ -9,6 +9,7 @@ public class ServiceConstants {
     public static final String COMPANY_NOT_FOUND_MESSAGE = "Company not found";
     public static final String COUNTRY_NOT_FOUND_MESSAGE = "Country not found";
     public static final String GENERAL_STATE_NOT_FOUND_MESSAGE = "General state not found";
+    public static final String PAYMENT_TYPE_NOT_FOUND_MESSAGE = "Payment type not found";
     public static final String QUEUE_NOT_FOUND_MESSAGE = "Queue not found";
     public static final String INFLUENCING_TICKET_NOT_FOUND_MESSAGE = "Influencing ticket not found";
     public static final String TARIFF_NOT_FOUND_MESSAGE = "Tariff not found";
