7ce027c05263cf873fd4b4a2ff34afc924bd0f90
Martin Patyk <martin.patyk@telia.no>
2022-04-21 21:38:35 +0200

    updated PaymentTypeDto

--- src/main/java/cz/patyk/invoicesystem_be/dto/PaymentTypeDto.java
+++ src/main/java/cz/patyk/invoicesystem_be/dto/PaymentTypeDto.java
@@ -2,13 +2,32 @@ package cz.patyk.invoicesystem_be.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
-public class PaymentTypeDto {
+public class PaymentTypeDto extends RepresentationModel<PaymentTypeDto> {
     private Long id;
     private String name;
     private boolean isDefault;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        PaymentTypeDto that = (PaymentTypeDto) o;
+        return isDefault == that.isDefault && Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, name, isDefault);
+    }
 }
