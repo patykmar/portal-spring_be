cbd76b16ca404435a6b678684d39a6afe4a15dde
Martin Patyk <martin.patyk@telia.no>
2022-04-24 14:32:42 +0200

    updated UserService

--- src/main/java/cz/patyk/invoicesystem_be/service/UserService.java
+++ src/main/java/cz/patyk/invoicesystem_be/service/UserService.java
@@ -1,13 +1,15 @@
 package cz.patyk.invoicesystem_be.service;
 
-import cz.patyk.invoicesystem_be.dto.UserDto;
+import cz.patyk.invoicesystem_be.dto.in.UserDtoIn;
+import cz.patyk.invoicesystem_be.dto.in.UserDtoInTwoPassword;
+import cz.patyk.invoicesystem_be.dto.out.UserDtoOut;
 import cz.patyk.invoicesystem_be.dto.in.UserPasswordChangeIn;
 import cz.patyk.invoicesystem_be.entities.User;
 import cz.patyk.invoicesystem_be.mapper.UserMapper;
 import cz.patyk.invoicesystem_be.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -20,51 +22,57 @@ import static cz.patyk.invoicesystem_be.service.ServiceConstants.USER_NOT_FOUND_
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-    private final PasswordEncoder passwordEncoder;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final ErrorHandleService errorHandleService;
 
-    public List<UserDto> getAll(Pageable pageable) {
+    public List<UserDtoOut> getAll(Pageable pageable) {
         return userRepository.findAll(pageable)
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
     }
 
-    public UserDto getOne(Long id) {
+    public UserDtoOut getOne(Long id) {
         return userMapper.toDto(userRepository.findById(id)
                 .orElseThrow(() -> errorHandleService.handleNotFoundError(id, USER_NOT_FOUND_MESSAGE))
         );
     }
 
-    public UserDto newItem(UserDto userDto) {
-        User user = userMapper.toEntity(userDto);
+    public UserDtoOut newItem(UserDtoInTwoPassword userDtoInTwoPassword) {
+        if (!userDtoInTwoPassword.getPassword().equals(userDtoInTwoPassword.getRetypePassword())) {
+            throw errorHandleService.handleBadRequestError("New password and re-typed password is not match.");
+        }
+        User user = userMapper.toEntity(userMapper.toDtoIn(userDtoInTwoPassword));
+        user.setPassword(passwordEncode(userDtoInTwoPassword.getPassword()));
+
         return userMapper.toDto(userRepository.save(user));
     }
 
-    public UserDto editItem(UserDto userDto, Long id) {
+    public UserDtoOut editItem(UserDtoIn userDtoIn, Long id) {
         isIdExist(id);
-        User user = userMapper.toEntity(userDto);
+        User user = userMapper.toEntity(userDtoIn);
         user.setId(id);
         return userMapper.toDto(userRepository.save(user));
     }
 
-    public void passwordChange(UserPasswordChangeIn userPasswordChangeIn, Long id) {
+    public UserDtoOut passwordChange(UserPasswordChangeIn userPasswordChangeIn, Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> errorHandleService.handleNotFoundError(id, USER_NOT_FOUND_MESSAGE));
 
-        if(!userPasswordChangeIn.getNewPassword().equals(userPasswordChangeIn.getReTypedPassword())){
+        if (!userPasswordChangeIn.getNewPassword().equals(userPasswordChangeIn.getReTypedPassword())) {
             throw errorHandleService.handleBadRequestError("New password and re-typed password is not match.");
         }
 
-        if (!passwordEncoder.matches(userPasswordChangeIn.getOldPassword(), user.getPassword())) {
+        if (!bCryptPasswordEncoder.matches(userPasswordChangeIn.getOldPassword(), user.getPassword())) {
             throw errorHandleService.handleBadRequestError(id, "Your old password is incorrect.");
         } else {
             changePassword(user, userPasswordChangeIn.getNewPassword());
         }
+        return userMapper.toDto(user);
     }
 
     public String passwordEncode(String clearPassword) {
-        return passwordEncoder.encode(clearPassword);
+        return bCryptPasswordEncoder.encode(clearPassword);
     }
 
     public void deleteItem(Long id) {
@@ -79,7 +87,7 @@ public class UserService {
     }
 
     private void changePassword(User user, String newPassword) {
-        user.setPassword(passwordEncoder.encode(newPassword));
+        user.setPassword(bCryptPasswordEncoder.encode(newPassword));
         user.setPasswordChanged(Instant.now().getEpochSecond());
         userRepository.save(user);
     }
