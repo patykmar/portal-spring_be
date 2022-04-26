76c5b5eea6d3aa3f5a21777c6db157ddd926fff7
Martin Patyk <martin.patyk@telia.no>
2022-04-26 16:04:05 +0200

    init PaymentTypeControllerTest

---
+++ src/test/java/cz/patyk/invoicesystem_be/controllers/PaymentTypeControllerTest.java
@@ -0,0 +1,28 @@
+package cz.patyk.invoicesystem_be.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PaymentTypeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    //https://www.baeldung.com/spring-boot-bean-validation
+    @Test
+    void getAll() throws Exception {
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/payment-types").contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
