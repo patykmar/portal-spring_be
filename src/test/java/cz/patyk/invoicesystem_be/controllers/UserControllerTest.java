ffb0468445c6bc47b6199e69f883b18e4d5e54ba
Martin Patyk <martin.patyk@telia.no>
2022-05-05 00:22:10 +0200

    init UserControllerTest

---
+++ src/test/java/cz/patyk/invoicesystem_be/controllers/UserControllerTest.java
@@ -0,0 +1,39 @@
+package cz.patyk.invoicesystem_be.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class UserControllerTest {
+
+    @Test
+    void getAll() {
+        assertTrue(true);
+    }
+
+    @Test
+    void getOne() {
+        assertTrue(true);
+    }
+
+    @Test
+    void newItem() {
+        assertTrue(true);
+    }
+
+    @Test
+    void editItem() {
+        assertTrue(true);
+    }
+
+    @Test
+    void testEditItem() {
+        assertTrue(true);
+    }
+
+    @Test
+    void deleteItem() {
+        assertTrue(true);
+    }
+}
