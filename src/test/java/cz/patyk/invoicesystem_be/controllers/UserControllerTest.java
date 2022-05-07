28c53db8f2120b5a0a2b33d4b8599c6216d87a40
Martin Patyk <martin.patyk@telia.no>
2022-05-08 00:50:28 +0200

    updated UserControllerTest

--- src/test/java/cz/patyk/invoicesystem_be/controllers/UserControllerTest.java
+++ src/test/java/cz/patyk/invoicesystem_be/controllers/UserControllerTest.java
@@ -1,7 +1,9 @@
 package cz.patyk.invoicesystem_be.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import cz.patyk.invoicesystem_be.dto.out.UserDtoOut;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -12,12 +14,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.stream.Stream;
 
 import static cz.patyk.invoicesystem_be.common.TestDtoConstants.USER_DTO_IN_ROLE_USER;
+import static cz.patyk.invoicesystem_be.common.TestDtoConstants.USER_DTO_IN_ROLE_USER_WITH_PASSWORD;
 import static org.apache.commons.lang3.math.NumberUtils.LONG_MINUS_ONE;
 import static org.apache.commons.lang3.math.NumberUtils.LONG_ZERO;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -39,6 +43,41 @@ class UserControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get(URL).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaTypes.HAL_JSON_VALUE));
+    }
+
+    @Test
+    @DisplayName("Testing API HTTP methods POST, PUT and DELETE")
+    void testApi() throws Exception {
+        String userDtoInRoleUserWithPassword = objectMapper.writeValueAsString(USER_DTO_IN_ROLE_USER_WITH_PASSWORD);
+
+        log.info("==== Testing post method: creating new User ====");
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userDtoInRoleUserWithPassword))
+
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaTypes.HAL_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.employeeOfCompanyId").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.employeeOfCompanyId").value(USER_DTO_IN_ROLE_USER_WITH_PASSWORD.getEmployeeOfCompanyId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").isString())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(USER_DTO_IN_ROLE_USER_WITH_PASSWORD.getEmail()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").isString())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value(USER_DTO_IN_ROLE_USER_WITH_PASSWORD.getFirstName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").isString())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value(USER_DTO_IN_ROLE_USER_WITH_PASSWORD.getLastName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastLogin").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.createdDate").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.passwordChanged").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.roles").isString())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.roles").value(USER_DTO_IN_ROLE_USER_WITH_PASSWORD.getRoles()))
+                .andReturn();
+
+        UserDtoOut userDtoOut = objectMapper.readValue(
+                mvcResult.getResponse().getContentAsString(),
+                UserDtoOut.class
+        );
     }
 
     @Test
@@ -64,6 +103,16 @@ class UserControllerTest {
     @Test
     void deleteItem() {
         assertTrue(true);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidIds")
+    void testGetOneWithInvalidId(Long id) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(URL + "/" + id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+        ;
     }
 
     @ParameterizedTest
