d121dd2150498c6abb695c48a5f102cfe6e8773d
Martin Patyk <martin.patyk@telia.no>
2022-05-26 23:52:32 +0200

    created passwordChangeWithWrongOldPassword method

--- src/test/java/cz/patyk/invoicesystem_be/service/UserServiceTest.java
+++ src/test/java/cz/patyk/invoicesystem_be/service/UserServiceTest.java
@@ -1,28 +1,37 @@
 package cz.patyk.invoicesystem_be.service;
 
+import cz.patyk.invoicesystem_be.dto.in.UserPasswordChangeIn;
+import cz.patyk.invoicesystem_be.exceptions.ApplicationException;
 import cz.patyk.invoicesystem_be.mapper.UserMapper;
 import cz.patyk.invoicesystem_be.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 import org.mockito.Mockito;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Optional;
+
 import static cz.patyk.invoicesystem_be.common.TestConstants.USER_TEST_PASSWORD;
-import static org.junit.jupiter.api.Assertions.*;
+import static cz.patyk.invoicesystem_be.common.TestEntityConstants.USER_ENTITY;
+import static cz.patyk.invoicesystem_be.service.ServiceConstants.USER_INCORRECT_OLD_PASSWORD;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 class UserServiceTest {
     private static BCryptPasswordEncoder bCryptPasswordEncoder;
     private static UserService userService;
+    private static UserRepository userRepository;
 
     @BeforeAll
-    static void initTest(){
-        UserRepository userRepository = Mockito.mock(UserRepository.class);
+    static void initTest() {
+        userRepository = Mockito.mock(UserRepository.class);
         UserMapper userMapper = Mappers.getMapper(UserMapper.class);
         bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        ErrorHandleService errorHandleService = Mockito.mock(ErrorHandleService.class);
+        ErrorHandleService errorHandleService = new ErrorHandleService();
 
         userService = new UserService(userRepository, userMapper, bCryptPasswordEncoder, errorHandleService);
     }
@@ -30,7 +39,20 @@ class UserServiceTest {
     @Test
     void passwordEncode() {
         String passwordEncode = userService.passwordEncode(USER_TEST_PASSWORD);
-        log.info("Password encode: {}", passwordEncode);
+        log.debug("Password encode: {}", passwordEncode);
         assertTrue(bCryptPasswordEncoder.matches(USER_TEST_PASSWORD, passwordEncode));
+    }
+
+    @Test
+    void passwordChangeWithWrongOldPassword() {
+        //TODO moved builder to constants class
+        UserPasswordChangeIn userPasswordChangeIn = UserPasswordChangeIn.builder().oldPassword("OldPassword").newPassword("NewPassword").reTypedPassword("NewPassword").build();
+        Mockito
+                .when(userRepository.findById(NumberUtils.LONG_ONE))
+                .thenReturn(Optional.ofNullable(USER_ENTITY));
+
+        assertThatThrownBy(() -> userService.passwordChange(userPasswordChangeIn, NumberUtils.LONG_ONE))
+                .isInstanceOf(ApplicationException.class)
+                .hasMessageContaining(USER_INCORRECT_OLD_PASSWORD);
     }
 }
