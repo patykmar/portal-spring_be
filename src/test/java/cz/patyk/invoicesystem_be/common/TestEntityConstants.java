d2ec2789fc78e355e67b7d1ad6bef5f8aa8a2693
Martin Patyk <martin.patyk@telia.no>
2022-05-26 23:50:36 +0200

    update TestEntityConstants

--- src/test/java/cz/patyk/invoicesystem_be/common/TestEntityConstants.java
+++ src/test/java/cz/patyk/invoicesystem_be/common/TestEntityConstants.java
@@ -1,10 +1,20 @@
 package cz.patyk.invoicesystem_be.common;
 
 import cz.patyk.invoicesystem_be.entities.PaymentType;
+import cz.patyk.invoicesystem_be.entities.User;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import static cz.patyk.invoicesystem_be.common.TestConstants.COMPANY_TEST_ENTITY;
 import static cz.patyk.invoicesystem_be.common.TestConstants.PAYMENT_TYPE_TEST_NAME;
+import static cz.patyk.invoicesystem_be.common.TestConstants.USER_TEST_CREATED;
+import static cz.patyk.invoicesystem_be.common.TestConstants.USER_TEST_EMAIL;
+import static cz.patyk.invoicesystem_be.common.TestConstants.USER_TEST_FIRST_NAME;
+import static cz.patyk.invoicesystem_be.common.TestConstants.USER_TEST_LAST_LOGIN;
+import static cz.patyk.invoicesystem_be.common.TestConstants.USER_TEST_LAST_NAME;
+import static cz.patyk.invoicesystem_be.common.TestConstants.USER_TEST_PASSWORD_CHANGED;
+import static cz.patyk.invoicesystem_be.common.TestConstants.USER_TEST_PASSWORD_ENCODE;
+import static org.apache.commons.lang3.math.NumberUtils.LONG_ONE;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TestEntityConstants {
@@ -12,4 +22,12 @@ public class TestEntityConstants {
             .name(PAYMENT_TYPE_TEST_NAME).isDefault(false).build();
     public static final PaymentType PAYMENT_TYPE_ENTITY_EDIT = PaymentType.builder()
             .name(PAYMENT_TYPE_TEST_NAME + " edited").isDefault(true).build();
+
+    public static final User USER_ENTITY = User.builder()
+            .id(LONG_ONE).firstName(USER_TEST_FIRST_NAME)
+            .lastName(USER_TEST_LAST_NAME).email(USER_TEST_EMAIL)
+            .employeeOfCompanyId(COMPANY_TEST_ENTITY)
+            .roles(User.Role.USER).password(USER_TEST_PASSWORD_ENCODE)
+            .lastLogin(USER_TEST_LAST_LOGIN).createdDate(USER_TEST_CREATED)
+            .passwordChanged(USER_TEST_PASSWORD_CHANGED).build();
 }
