0b0b92b0afeeab320da66188a121f41706084727
Martin Patyk <martin.patyk@telia.no>
2022-06-07 12:51:45 +0200

    updated ServiceCatalogMapper

--- src/main/java/cz/patyk/invoicesystem_be/mapper/ServiceCatalogMapper.java
+++ src/main/java/cz/patyk/invoicesystem_be/mapper/ServiceCatalogMapper.java
@@ -1,12 +1,26 @@
 package cz.patyk.invoicesystem_be.mapper;
 
-import cz.patyk.invoicesystem_be.dto.ServiceCatalogDto;
+import cz.patyk.invoicesystem_be.dto.in.ServiceCatalogDtoIn;
+import cz.patyk.invoicesystem_be.dto.out.ServiceCatalogDtoOut;
 import cz.patyk.invoicesystem_be.entities.ServiceCatalog;
+import cz.patyk.invoicesystem_be.entities.Vat;
+import cz.patyk.invoicesystem_be.service.VatService;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring")
-public interface ServiceCatalogMapper {
-    ServiceCatalog toEntity(ServiceCatalogDto serviceCatalogDto);
+@Mapper(componentModel = "spring", uses = {VatMapper.class})
+public abstract class ServiceCatalogMapper {
+    @Autowired
+    protected VatService vatService;
 
-    ServiceCatalogDto toDto(ServiceCatalog serviceCatalog);
+    @Mapping(target = "vat", expression = "java(getVat(serviceCatalogDtoIn.getVat()))")
+    public abstract ServiceCatalog toEntity(ServiceCatalogDtoIn serviceCatalogDtoIn);
+
+    @Mapping(target = "vat", source = "vat.id")
+    public abstract ServiceCatalogDtoOut toDtoOut(ServiceCatalog serviceCatalog);
+
+    public Vat getVat(Long vat){
+        return vatService.getEntityById(vat);
+    }
 }
