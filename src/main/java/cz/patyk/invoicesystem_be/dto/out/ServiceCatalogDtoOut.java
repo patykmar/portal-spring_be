0d25177fee4236f32442f49041561dff98904155
Martin Patyk <martin.patyk@telia.no>
2022-06-12 02:24:58 +0200

    updated ServiceCatalogDtoOut

--- src/main/java/cz/patyk/invoicesystem_be/dto/out/ServiceCatalogDtoOut.java
+++ src/main/java/cz/patyk/invoicesystem_be/dto/out/ServiceCatalogDtoOut.java
@@ -1,13 +1,16 @@
 package cz.patyk.invoicesystem_be.dto.out;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
 @Builder
-@AllArgsConstructor
-public class ServiceCatalogDtoOut {
+public class ServiceCatalogDtoOut extends RepresentationModel<ServiceCatalogDtoOut> {
     private Long id;
     private String name;
     private String description;
@@ -17,4 +20,28 @@ public class ServiceCatalogDtoOut {
     private int estimateTimeDelivery;
     private int estimateTimeReaction;
     private boolean isDisable;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ServiceCatalogDtoOut that = (ServiceCatalogDtoOut) o;
+        return estimateTimeDelivery == that.estimateTimeDelivery &&
+                estimateTimeReaction == that.estimateTimeReaction &&
+                isDisable == that.isDisable &&
+                Objects.equals(id, that.id) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(price, that.price) &&
+                Objects.equals(vat, that.vat) &&
+                Objects.equals(vatDtoOut, that.vatDtoOut);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, name, description, price, vat, vatDtoOut, estimateTimeDelivery, estimateTimeReaction, isDisable);
+    }
 }
+
+
