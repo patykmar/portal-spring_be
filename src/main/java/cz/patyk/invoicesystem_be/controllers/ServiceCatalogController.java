b45e92cecf64ca4e2554ba5a15388d8b712f821e
Martin Patyk <martin.patyk@telia.no>
2022-07-07 15:42:06 +0200

    added method newItem

--- src/main/java/cz/patyk/invoicesystem_be/controllers/ServiceCatalogController.java
+++ src/main/java/cz/patyk/invoicesystem_be/controllers/ServiceCatalogController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,6 +52,13 @@ public class ServiceCatalogController {
         return ResponseEntity.ok(serviceCatalogService.getOne(id));
     }
 
+    @PostMapping("")
+    public ResponseEntity<ServiceCatalogDtoOut> newItem(
+            @Valid @RequestBody ServiceCatalogDtoIn serviceCatalogDtoIn
+    ) {
+        return ResponseEntity.ok(serviceCatalogService.newItem(serviceCatalogDtoIn));
+    }
+
     @PutMapping("/{id}")
     public ResponseEntity<ServiceCatalogDtoOut> editItem(
             @Valid @RequestBody ServiceCatalogDtoIn serviceCatalogDtoIn,
@@ -62,7 +70,7 @@ public class ServiceCatalogController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteItem(
             @PathVariable Long id
-    ){
+    ) {
         serviceCatalogService.deleteItem(id);
         return ResponseEntity.noContent().build();
     }
