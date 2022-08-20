2413541cfa832e1da0c2af308de553971068af51
Martin Patyk <martin.patyk@telia.no>
2022-08-21 01:05:32 +0200

    Update sla mapper

--- src/main/java/cz/patyk/invoicesystem_be/mapper/SlaMapper.java
+++ src/main/java/cz/patyk/invoicesystem_be/mapper/SlaMapper.java
@@ -2,22 +2,29 @@ package cz.patyk.invoicesystem_be.mapper;
 
 import cz.patyk.invoicesystem_be.dto.in.SlaDtoIn;
 import cz.patyk.invoicesystem_be.dto.out.SlaDtoOut;
+import cz.patyk.invoicesystem_be.entities.InfluencingTicket;
 import cz.patyk.invoicesystem_be.entities.Sla;
 import cz.patyk.invoicesystem_be.entities.Tariff;
-import cz.patyk.invoicesystem_be.service.SlaService;
+import cz.patyk.invoicesystem_be.entities.TicketType;
+import cz.patyk.invoicesystem_be.service.InfluencingTicketService;
+import cz.patyk.invoicesystem_be.service.TariffService;
+import cz.patyk.invoicesystem_be.service.TicketTypeService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
-//@Mapper(componentModel = "spring", uses = {TicketTypeMapper.class, TariffMapper.class, InfluencingTicketMapper.class})
-@Mapper(componentModel = "spring", uses = {TicketTypeMapper.class})
+@Mapper(componentModel = "spring", uses = {TicketTypeMapper.class, TariffMapper.class, InfluencingTicketMapper.class})
 public abstract class SlaMapper {
     @Autowired
-    protected SlaService slaService;
+    protected TariffService tariffService;
+    @Autowired
+    protected InfluencingTicketService influencingTicketService;
+    @Autowired
+    protected TicketTypeService ticketTypeService;
 
-    @Mapping(source = "tariffId", target = "tariff.id")
-    @Mapping(source = "priorityId", target = "priority.id")
-    @Mapping(source = "ticketTypeId", target = "ticketType.id")
+    @Mapping(target = "tariff", expression = "java(getTariff(slaDto.getTariffId()))")
+    @Mapping(target = "priority", expression = "java(getPriority(slaDto.getPriorityId()))")
+    @Mapping(target = "ticketType", expression = "java(getTicketTypeId(slaDto.getTicketTypeId()))")
     public abstract Sla toEntity(SlaDtoIn slaDto);
 
     @Mapping(source = "tariff.id", target = "tariffId")
@@ -25,7 +32,15 @@ public abstract class SlaMapper {
     @Mapping(source = "ticketType.id", target = "ticketTypeId")
     public abstract SlaDtoOut toDtoOut(Sla sla);
 
-    public Sla getSla(Long sla) {
-        return slaService.getEntityById(sla);
+    protected Tariff getTariff(Long id) {
+        return tariffService.getOneEntity(id);
+    }
+
+    protected InfluencingTicket getPriority(Long id) {
+        return influencingTicketService.getOneEntity(id);
+    }
+
+    protected TicketType getTicketTypeId(Long id) {
+        return ticketTypeService.getOneEntity(id);
     }
 }
