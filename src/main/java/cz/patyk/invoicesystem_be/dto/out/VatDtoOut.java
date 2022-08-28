bc61bb7a3ca98830f06bff0aa6c9ccb3fa6512b6
Martin Patyk <martin.patyk@telia.no>
2022-08-28 17:50:57 +0200

    VatDtoOut updated lombok

--- src/main/java/cz/patyk/invoicesystem_be/dto/out/VatDtoOut.java
+++ src/main/java/cz/patyk/invoicesystem_be/dto/out/VatDtoOut.java
@@ -1,21 +1,17 @@
 package cz.patyk.invoicesystem_be.dto.out;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
 
-@Getter
-@Setter
+@Data
 @Builder
-@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class VatDtoOut extends RepresentationModel<VatDtoOut> {
-    private Long id;
-    private boolean isDefault;
-    private String name;
-    private int percent;
-    private int multiplier;
+    private final Long id;
+    private final boolean isDefault;
+    private final String name;
+    private final int percent;
+    private final int multiplier;
 }
